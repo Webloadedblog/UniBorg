4e309f4c137083cab7bcab344af24f66cce2460a
Cee Jay <52139555+Somto811@users.noreply.github.com>
2020-02-03 00:07:08 +0100

    Update clk.py

--- stdplugins/clk.py
+++ stdplugins/clk.py
@@ -8,7 +8,7 @@ from uniborg.util import admin_cmd
 @borg.on(admin_cmd(pattern="enaclk ?(.*)", outgoing=True))
 async def enaclk(event):
     await event.edit("K...")
-@borg.on(events.NewMessage(pattern="http)",incoming=True, func=lambda e: e.is_private))
+@borg.on(events.NewMessage(pattern="http",incoming=True, func=lambda e: e.is_private))
       async def clkstart(m):
         person = await m.get_sender()
         user=person.first_name
