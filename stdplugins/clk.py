35afe39f9e36b71f313e715611098ca77af5ce10
Cee Jay <52139555+Somto811@users.noreply.github.com>
2020-02-02 02:50:21 +0100

    Update clk.py

--- stdplugins/clk.py
+++ stdplugins/clk.py
@@ -5,13 +5,13 @@ import requests
 from random import randint 
 from uniborg.util import admin_cmd
   
-@borg.on(pattern=r"^.enaclk", outgoing=True)
+@borg.on(pattern="enaclk ?(.*)", outgoing=True))
 async def enaclk(event):
     await event.edit("K...")
-    @borg.on(pattern=r"^http",incoming=True, func=lambda e: e.is_private)
+    @borg.on(pattern="http",incoming=True, func=lambda e: e.is_private)
     async def clkstart(m):
         person = await m.get_sender()
-        user=person.first_name
+        user=person.firstpattern="download ?(.*)_name
         rantext = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
         api_token = '32f9105a8194b5596482f0ed9631ab1483ec171e'
         req = requests.get('https://ilinkshort.com/api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
