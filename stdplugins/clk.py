b9e86b352c4722f8913f865a35019ae5360dbed1
Cee Jay <52139555+Somto811@users.noreply.github.com>
2020-02-02 17:59:54 +0100

    Update clk.py

--- stdplugins/clk.py
+++ stdplugins/clk.py
@@ -9,7 +9,7 @@ from uniborg.util import admin_cmd
 async def enaclk(event):
     await event.edit("K...")
 @borg.on(events.NewMessage(pattern="http",incoming=True, func=lambda e: e.is_private)
-       async def clkstart(event):
+       async def handler(event):
         person = await m.get_sender()
         user=person.first_name
         rantext = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
