ef891cfe03bf7a0d91db9a0b44c663e227db2d3f
Cee Jay <52139555+Somto811@users.noreply.github.com>
2020-02-10 01:39:26 +0000

    Create Mychannel.py

---
+++ stdplugins/Mychannel.py
@@ -0,0 +1,10 @@
+All thanks to snapdragon & loxxi
+
+from telethon import events
+
+@borg.on(events.NewMessage(pattern="", incoming=True))
+async def transfer(event):
+    target_channel = 1147697612
+    my_channel = 1276100737
+    if event.chat_id == target_channel:
+        await borg.send_message(my_channel, event.message)
