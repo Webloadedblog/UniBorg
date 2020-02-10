84982383a370139c563dc27ae248154019fb3ee9
Cee Jay <52139555+Somto811@users.noreply.github.com>
2020-02-10 01:53:30 +0000

    Create clk.py

---
+++ stdplugins/clk.py
@@ -0,0 +1,44 @@
+import random
+
+import string
+
+import requests
+
+from random import randint
+
+from uniborg.util import admin_cmd
+
+ 
+
+@borg.on(admin_cmd(pattern="enaclk ?(.*)", outgoing=True))
+
+async def enaclk(event):
+
+    await event.edit("K...")
+
+@borg.on(events.NewMessage(pattern="http",incoming=True)) 
+       async def clkstart(m):
+
+        person = await m.get_sender()
+
+        user=person.first_name
+
+        rantext = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
+
+        api_token = '32f9105a8194b5596482f0ed9631ab1483ec171e'
+
+        req = requests.get('https://ilinkshort.com/api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
+
+        if(req["status"] == 'error'):
+
+          smsg = req["message"]
+
+        else:
+
+          smsg = req["shortenedUrl"]
+
+     
+
+        sent = await m.reply(smsg)
+
+    await event.edit("Done...")
