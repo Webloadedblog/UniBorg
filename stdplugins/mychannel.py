09a72bdd61deceb5b358860f86862d5e17345c42
Cee Jay <52139555+Somto811@users.noreply.github.com>
2020-02-11 17:34:39 +0000

    Update mychannel.py

--- stdplugins/mychannel.py
+++ stdplugins/mychannel.py
@@ -1,8 +1,8 @@
 from telethon import events
 
-@borg.on(events.NewMessage(pattern="", incoming=True))
+@borg.on(events.NewMessage(incoming=True))
 async def transfer(event):
-    target_channel = -1147697612
-    my_channel = -1276100737
+    target_channel = -1001389391900
+    my_channel = -1001276100737
     if event.chat_id == target_channel:
         await borg.send_message(my_channel, event.message)
